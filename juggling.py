288308ffc07f5d48ac4e3256fe74bc414aefbec3
Lam Yoke Yu <147635499+Yuylam@users.noreply.github.com>
2024-01-06 15:11:10 +0800

    Update juggling.py

--- juggling.py
+++ juggling.py
@@ -17,11 +17,7 @@ while True:
         break
 
     frame = rescaleFrame(frame, scale=0.7)
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blurred_gray = cv.medianBlur(gray, 7)
-    blurred_frame = cv.cvtColor(blurred_gray, cv.COLOR_GRAY2BGR)
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    rows = blurred_gray.shape[0]
 
     lowerLimit1 = np.array([0, 200, 20])
     upperLimit1 = np.array([5, 255, 255])
@@ -30,17 +26,10 @@ while True:
     mask1 = cv.inRange(hsv, lowerLimit1, upperLimit1)
     mask2 = cv.inRange(hsv, lowerLimit2, upperLimit2)
     mask = cv.bitwise_or(mask1, mask2)
+    res = cv.bitwise_and(frame,frame, mask = mask)
+    gray = cv.cvtColor(res,cv.COLOR_BGR2GRAY)
 
-    # haar_cascade = cv.CascadeClassifier('haar_face.xml')
-    # faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
-    # for (x, y, w, h) in faces:
-    #     mask[y:y+h, x:x+w] = 0
-
-    blurred_mask = cv.GaussianBlur(mask, (5, 5), 0)
-
-    thresh = cv.adaptiveThreshold(blurred_mask, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-
-    circles = cv.HoughCircles(thresh, cv.HOUGH_GRADIENT, 1, rows/8,param1=50, param2=30, minRadius=10, maxRadius=0)
+    circles = cv.HoughCircles(gray,cv.HOUGH_GRADIENT,3, 70, param1=400,param2=40,minRadius=0,maxRadius=40)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
@@ -55,8 +44,8 @@ while True:
         cv.putText(frame, 'Balls: 0', (10, 15), cv.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), thickness=1)
 
     cv.imshow('Video', frame)
-    cv.imshow('HSV', hsv)
-    cv.imshow('Mask', mask2)
+    # cv.imshow('HSV', hsv)
+    cv.imshow('Mask', res)
 
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
